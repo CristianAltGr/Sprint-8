204ca1551d158cba3538e5fba50ff893d0aedc18
Cristian <crifergon1@gmail.com>
2022-09-15 17:17:47 +0200

    exercici1 do it!

--- src/pages/ships.js
+++ src/pages/ships.js
@@ -1,3 +1,32 @@
-const Ships = () => <div>ships</div>;
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+
+const Ships = () => {
+
+    const [ships, setShips] = useState([]);
+
+    useEffect(() => {
+        //const request: Joke = await fetch(APIJOKE,{headers: {'Accept': 'application/json'}})
+        //.then((res) => res.json())
+        axios.get("https://swapi.dev/api/starships/").then((res) => { setShips(res.data.results) });
+
+    }, []);
+
+    return (
+
+        <div>
+            {ships.map(ship => {
+                return (
+                    <div key={ship.created}>
+                        <h4>{ship.name}</h4>
+                        <p> {ship.model}</p>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
 
 export default Ships;
